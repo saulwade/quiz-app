2d63161d534a8de2fc60505e90b05ba3a4675916
saulwade <saulwade29@gmail.com>
2023-01-04 01:41:37 -0600

    web application done

---
+++ README.md
@@ -0,0 +1,7 @@
+In this project I used multiple JavaScript features to create a great quiz app. It only has 5 question but I can add more in the future.
+
+Here is the link of the App:
+
+In this is a preview:
+
+![Getting Started](./images/preview.jpg)
---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the name for the basic orders given to a computer?",
+    answer: "Code",
+    options: [
+      "Algorithm",
+      "Code",
+      "Line",
+      "Petition"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a list of steps you can follow to finish a task?",
+    answer: "Algorithm",
+    options: [
+      "Algorithm",
+      "Program",
+      "Code",
+      "Bug"
+    ]
+  },
+
+];
